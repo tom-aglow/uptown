0af4b182d181a38fd03c97ba987b482eea257a6d
tom aglow <tom@ohhhh.me>
2017-08-29 22:01:10 -0700

    added about section

--- app/assets/javascripts/public/public.js
+++ app/assets/javascripts/public/public.js
@@ -1,7 +1,11 @@
 $(function () {
+    // Mobile navber toggling
     $(".navbar-mobile-toggle").click(function () {
         $(this).toggleClass("is-open");
         $('.navbar-mobile').toggleClass("is-open");
     });
+
+    // Testimonial carousel
+    $('.about__testimonials .carousel').carousel();
 });
 
