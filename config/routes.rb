63f6c76a6cd5875d83114ea64f9d0cc5aa352292
tom aglow <tom@ohhhh.me>
2017-08-30 16:53:52 -0700

    added template for reservation form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'public#index'
 
-  get 'public/index'
-  get 'public/reservation'
+  get 'index', to: 'public#index'
+  get 'reservation', to: 'public#reservation'
 
 end
