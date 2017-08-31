887758296520390f204f7641acd2c8c951a62f73
tom aglow <tom@ohhhh.me>
2017-08-31 14:54:02 -0700

    added date picker and linked it to the form field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
+//= require bootstrap-datepicker
 //= require public/public
 // require_tree .
--- app/assets/javascripts/public/reservation-page.js
+++ app/assets/javascripts/public/reservation-page.js
@@ -2,6 +2,20 @@ $(function () {
     let barbersImages = $('.barbers__img__cont');
     let barbersNames = $('.barbers__names span');
 
+    //  DATEPICKER
+    let dateInput = $('#datepicker')
+    dateInput.datepicker({
+        format: 'yyyy-mm-dd',
+        orientation: 'left bottom',
+        startDate: new Date(),
+        todayHighlight: true,
+        datesDisabled: '2017-09-05'
+    });
+
+    dateInput.datepicker().on('changeDate', function () {
+        $('#shift_date').val(formatDate(dateInput.datepicker('getDate')));
+    });
+
     //  CLICK EVENT ON BARBER IMAGE & BARBER NAME
     barbersImages.click(function () {
         let index = barbersImages.index($(this));
@@ -13,7 +27,7 @@ $(function () {
         selectBarber(index);
     });
 
-    //  EVENTS WHEN STEP 2 BOXES CHANGES THEIR VALUE > UPDATE VALUES IN CORRESPONDENT FORM INPUTS
+    //  EVENTS WHEN STEP 2 BOXES CHANGE THEIR VALUE > UPDATE VALUES IN CORRESPONDENT FORM INPUTS
     $('#select-service').change(function () {
         $('#requisition_service_id').val($(this).val());
     });
@@ -31,5 +45,17 @@ $(function () {
         //  add active class only for target element
         barbersImages.eq(index).addClass('active');
         barbersNames.eq(index).addClass('active');
+    }
+
+    function formatDate (date) {
+        let d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [year, month, day].join('-');
     }
 });
