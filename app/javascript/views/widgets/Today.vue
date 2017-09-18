562cd13e81332a6ce396e72451d991d86e0cfb3d
tom-aglow <tom@ohhhh.me>
2017-09-17 18:28:37 -0700

    populate table content with dummy data for each time step

--- app/javascript/views/widgets/Today.vue
+++ app/javascript/views/widgets/Today.vue
@@ -3,6 +3,7 @@
         <div class="table">
             <div>Time</div>
             <div v-for="barber in data" v-text="barber.first_name"></div>
+            <div v-for="spot in schedule" v-text="spot"></div>
         </div>
     </div>
 </template>
@@ -11,7 +12,9 @@
     export default {
       data() {
         return {
-          data: false
+          data: false,
+          times: this.setTimes(),
+          schedule: false
         }
       },
 
@@ -21,11 +24,40 @@
 
       methods: {
         fetch() {
-          axios.get('/api/w-today').then(this.refresh);
+          axios.get('/api/w-today').then(this.refresh).then(this.setSchedule);
         },
 
         refresh ({data}) {
           this.data = data.data;
+        },
+        
+        setSchedule() {
+          let schedule = [];
+          for (let time of this.times) {
+            schedule.push(this.formatAMPM(time));
+            for (let barber of this.data) {
+              schedule.push(barber.id);
+            }
+          }
+          this.schedule = schedule;
+        },
+
+        //  private
+        setTimes() {
+          let arr = [];
+          for (let i = 10; i <= 19; i++) {
+            arr.push(i + ':00:00');
+          }
+          return arr;
+        },
+
+        formatAMPM(time) {
+          let hours = +time.split(':')[0];
+          let ampm = hours >= 12 ? 'PM' : 'AM';
+          hours = hours % 12;
+          hours = hours ? hours : 12; // the hour '0' should be '12'
+          hours = (hours < 10) ? '0' + hours : hours;
+          return hours + ':00 ' + ampm;
         }
       }
     }
