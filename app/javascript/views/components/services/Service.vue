022c4fdd9aa7d9eef9dd165a9f17f5fc4e2934df
tom-aglow <tom@ohhhh.me>
2017-09-19 21:32:55 -0700

    introduce API namespace for controllers // make dedicated controller for Services // add NewService vue-component // add delete feature for services view

--- app/javascript/views/components/services/Service.vue
+++ app/javascript/views/components/services/Service.vue
@@ -4,13 +4,19 @@
             <input type="text" class="form-control form-control-sm" v-if="editing" v-model="service.name">
             <span v-else="editing" v-text="service.name"></span>
         </td>
-        <td class="price" v-text="service.price"></td>
+        <td class="price">
+            <input type="text" class="form-control form-control-sm" v-if="editing" v-model="service.price">
+            <span v-else="editing" v-text="service.price"></span>
+        </td>
         <td class="actions">
             <div class="form-group" v-if="editing">
                 <div class="btn btn-xs btn-primary" @click="update">Save</div>
                 <div class="btn btn-xs btn-secondary" @click="editing = false">Cancel</div>
             </div>
-            <button class="btn btn-info btn-xs" v-else="editing" @click="editing = true">Edit</button>
+            <div v-else="editing">
+                <button class="btn btn-warning btn-xs" @click="editing = true">Edit</button>
+                <button class="btn btn-danger btn-xs" @click="destroy">Delete</button>
+            </div>
         </td>
     </tr>
 </template>
@@ -29,11 +35,18 @@
 
     methods: {
       update() {
-        axios.patch('/api/v-services/' + this.service.id, {
+        axios.patch('/api/services/' + this.service.id, {
           service: this.service
         });
         this.editing = false;
         //  TODO flash message
+      },
+
+      destroy() {
+        axios.delete('/api/services/' + this.service.id);
+
+        //  fire an event for Services component
+        this.$emit('deleted', this.service.id);
       }
     }
 
